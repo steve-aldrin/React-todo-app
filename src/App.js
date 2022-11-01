73f027c6ec053c57b0197dd7e5d41cba36b3dd4e
Steve Aldrin <stevealdrin.r2021@vitstudent.ac.in>
2022-11-01 21:09:25 +0530

    Update style todo

--- src/App.js
+++ src/App.js
@@ -50,8 +50,13 @@ function cleardone() {
      <div className="text-center bg-teal-600" >
     <Todolist todos={todos} toggleTodo={toggleTodo}/>
     <input className="bg-slate-600 flex left-103" ref={todoref} type='text'/>
-    <button className="bg-slate-600" onClick={add} >Add To do</button>
-
+  
+    <button type="submit" onClick={add} class="inline-flex items-center justify-center space-x-2 py-3 px-4 border border-transparent text-sm font-medium rounded text-green-600 hover:text-green-700 bg-green-50 hover:bg-green-100 transition-colors">
+<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
+</svg>
+  <div>Add To do</div>
+</button>
     <button onClick={clearlist} className="bg-slate-600 flex"> Clear Done Task</button>
     <button onClick={cleardone} className="bg-slate-600 flex"> Clear List</button>
     <div className="text-center" >{todos.filter(todo=>!todo.complete).length} left to do</div></div>
