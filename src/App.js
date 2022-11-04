efa5fde5ca23b5c618e13c28ed9c174dc6325fb8
Steve Aldrin <stevealdrin.r2021@vitstudent.ac.in>
2022-11-04 18:59:42 +0530

    UI improvement

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function cleardone() {
     <Todolist style={Styles} todos={todos} toggleTodo={toggleTodo}/>
    
 
-    <div>  <label for="input1" class="text-sm text-lime-200 block mb-1 font-medium m-4"> Enter A Task </label>  <input type="text"  ref={todoref} name="input1" id="input1" class="bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700" placeholder="eg:Homework"/></div>
+    <div>  <label for="input1" class="text-lg text-lime-200 block mb-1 font-medium m-4"> Enter A Task </label>  <input type="text"  ref={todoref} name="input1" id="input1" class="text- bg-gray-100 border border-gray-200 rounded py-1 px-3 block focus:ring-blue-500 focus:border-blue-500 text-gray-700" placeholder="eg:Homework"/></div>
   
     <button type="submit" onClick={add} class="m-5 inline-flex items-center justify-center space-x-2 py-3 px-4 border border-transparent text-sm font-medium rounded text-green-600 hover:text-green-700 bg-green-50 hover:bg-green-100 transition-colors">
 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -62,7 +62,7 @@ function cleardone() {
 </svg>
   <div>Add To do</div>
 </button>
-    <button onClick={clearlist} className="space-x-2 py-3 px-4 border border-transparent text-sm font-medium rounded text-green-600 hover:text-green-700 bg-green-50 hover:bg-green-100 transition-colors "> Clear Done Task</button>
+    <button onClick={clearlist} className="space-x-2 py-2 px-2 border border-transparent text-sm font-medium rounded text-green-600 hover:text-green-700 bg-green-50 hover:bg-green-100 transition-colors "> Clear Done Task</button>
     <button onClick={cleardone} className="m-5 space-x-2 py-3 px-4 border border-transparent text-sm font-medium rounded text-green-600 hover:text-green-700 bg-green-50 hover:bg-green-100 transition-colors "> Clear List</button>
     
 
